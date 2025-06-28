8d98a6916f80846ed71f29cda28d27c8ea573be3
Nguyen Lan Anh <mllemist@192.168.0.101>
2025-06-29 03:44:45 +0700

    UI header

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/d4850ad1fe7388b3409a6d81bc61c4e9~tplv-tiktokx-cropcenter:300:300.webp?dr=14577&refresh_token=1f7bac19&x-expires=1751313600&x-signature=K9K3vbZ2HYlUejnN59jOQ5%2Frfhk%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=c1333099&idc=my"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyen Thi Lan</span>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>nguyenthilan</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
